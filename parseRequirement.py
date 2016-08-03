716ee799a401c40e922f2b0f54d55153d5bdca9c
zhanglin-zhou <zhanglin.zhou@foxmail.com>
2016-08-03 15:57:15 +0800

    update code

--- parseRequirement.py
+++ parseRequirement.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
-
+import sys
 import json
 import urllib2
 
-print " { \"agent\" : { \"os\" : \"win7\", \"build\" : \"4207301\" }, \"broker\" : { \"os\" : \"win2008\" }}"
+result = [{
+   "agent" : { "os"     : "win7",
+               "build"  : "4207301"},
+   "broker" : { "os"    : "win2008"},
+   "testcases" : ["case1", "case2"]
+},{
+   "agent" : { "os"     : "win10",
+               "build"  : "4207301"},
+   "broker" : { "os"    : "win2008"},
+   "testcases" : ["case3", "case4"]
+}];
+
+for i, r in enumerate(result):
+   with open(sys.argv[1]+"_"+str(i), 'w') as result_file:
+      json.dump(r, result_file)
+
