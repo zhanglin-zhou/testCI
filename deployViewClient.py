ea2bbba80bdd82f4ca1c52f0fbc5fdfbccd970f8
zhanglin-zhou <zhanglin.zhou@foxmail.com>
2016-08-03 15:32:03 +0800

    update code

--- deployViewClient.py
+++ deployViewClient.py
@@ -5,3 +5,9 @@ import json
 import urllib2
 
 print "trying to deploy view client build: ", str(sys.argv[1])
+print "resouces file: ", str(sys.argv[2]) 
+with open(sys.argv[2]) as data_file:    
+    data = json.load(data_file)
+
+print "provided resouces:"
+print json.dumps(data, indent=4, sort_keys=True)
