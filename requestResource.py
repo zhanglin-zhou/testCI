038fd27864203e3c76cd070669157532c0ca183b
zhanglin-zhou <zhanglin.zhou@foxmail.com>
2016-08-03 15:23:58 +0800

    update code

--- requestResource.py
+++ requestResource.py
@@ -10,3 +10,14 @@ with open(sys.argv[1]) as data_file:
 
 print "requested resource based on requirement:"
 print json.dumps(data, indent=4, sort_keys=True)
+
+resources = { "broker" : "10.112.118.187",
+              "agent"  : "agent1"
+            }
+
+with open(sys.argv[2], 'w') as resource_file:
+   json.dump(resources, resource_file)
+
+
+print "provided resources:"
+print json.dumps(resources, indent=4, sort_keys=True)
