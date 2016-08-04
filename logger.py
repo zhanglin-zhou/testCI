5b6871ebef528ab44a6ae539dc60e57928eca47b
zhanglin-zhou <zhanglin.zhou@foxmail.com>
2016-08-04 15:35:30 +0800

    update code

---
+++ logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+import json
+import urllib2
+
+print "trying to upload view client build: ", str(sys.argv[1])
+print "uploaded log to racetrack: ", str(sys.argv[2])
+
+
