b3be749401b046732e89a498709a848592857f79
zhanglin-zhou <zhanglin.zhou@foxmail.com>
2016-08-09 13:07:27 +0800

    update code

--- requestResource.py
+++ requestResource.py
@@ -11,17 +11,26 @@ with open(sys.argv[1]) as data_file:
 print "requested resource based on requirement:"
 print json.dumps(data, indent=4, sort_keys=True)
 
-resources = { "broker" : "10.112.118.187",
-              "os"     : ["10.11", "10.10"]
-            }
-
-output = {
-   "resources" : resources,
-   "testcases" : data["testcases"]
-}
+resources = [{
+   "agent"     : "agent1",
+   "broker"    : "10.112.118.187",
+   "testcases" : ["case1", "case2"],
+   "os"        : "10.11"
+},
+{  "agent"     : "agent2",
+   "broker"    : "10.112.118.187",
+   "testcases" : ["case3", "case4"],
+   "os"        : "10.11"
+},
+{
+   "agent"     : "agent3",
+   "broker"    : "10.112.118.187",
+   "testcases" : ["case5", "case6"],
+   "os"        : "10.12"
+}]
 
 with open(sys.argv[2], 'w') as out_file:
-   json.dump(output, out_file)
+   json.dump(resources, out_file)
 
 
 print "provided resources:"
