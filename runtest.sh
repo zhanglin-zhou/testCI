78ec10ccbd0d3e94a20a63ea1402ea0be9380bf4
zhanglin-zhou <zhanglin.zhou@foxmail.com>
2016-08-16 17:08:10 +0800

    update code

---
+++ runtest.sh
@@ -0,0 +1,10 @@
+export PATH=$PATH:~/jython2.7.0/bin/
+export CLASSPATH=/Applications/Sikuli-IDE.app/Contents/Resources/Java/sikuli-script.jar:~/mm-mysql-2.0.7.jar
+export TESTOS=mac
+launchctl setenv VMWARE_VIEW_DEBUG_LOGGING "1"
+launchctl setenv VMWARE_VIEW_USBD_LOG_OPTIONS "-o log:trace"
+chmod +w ./conf/automation.ini
+chmod +w ./conf/database.ini
+cp ./automation.ini.test ./conf/automation.ini
+cd ./lib
+jython executetests.py -n $1 -s mac_bat -i test2
