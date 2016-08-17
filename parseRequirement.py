ae7baaf3eb1fbef20dc2d2574ba6e3eb9126ca3f
zhanglin-zhou <zhanglin.zhou@foxmail.com>
2016-08-17 16:48:38 +0800

    update

--- parseRequirement.py
+++ parseRequirement.py
@@ -7,6 +7,24 @@ import urllib2
 
 """
    Currently we always return one kind of requirement.
+   ,
+   {
+      "os": "10.12", 
+      "was": "true",
+      "testcases": [
+         "mac_bat/test2"
+      ],
+      "resources" : {
+         "agents": [
+            {
+               "os": "any"
+            }
+         ], 
+         "broker": {
+            "os": "any"
+         }
+      }
+   }
 """
 
 spec = [
@@ -27,15 +45,14 @@ spec = [
       }
    },
    {
-      "os": "10.12", 
-      "was": "true",
+      "os": "10.11", 
       "testcases": [
-         "mac_bat/test2"
+         "viewUITests/vmtAboutWindow/testConnectToDesktop"
       ],
       "resources" : {
          "agents": [
             {
-               "os": "any"
+               "os": "win7"
             }
          ], 
          "broker": {
